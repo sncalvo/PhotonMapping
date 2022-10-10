d32f2c0d390abd9adeba658d076672c0b131a94b
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-10-10 00:05:59 -0300

    Fix using dynamic import for yaml

--- PhotonMapping/vendor/includes/yaml-cpp/dll.h
+++ PhotonMapping/vendor/includes/yaml-cpp/dll.h
@@ -7,6 +7,8 @@
 // Definition yaml_cpp_EXPORTS using to building YAML-CPP as dll/so library
 // (definition created by CMake or defined manually)
 
+#define YAML_CPP_STATIC_DEFINE
+
 #ifdef YAML_CPP_STATIC_DEFINE
 #  define YAML_CPP_API
 #  define YAML_CPP_NO_EXPORT
